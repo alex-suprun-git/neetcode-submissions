55c51c536059212d5ceedfefcaf04944bcacacef
Alex Suprun <alexey.suprun.mail@gmail.com>
2026-08-13 22:02:16 +0200

    Add: is-anagram - submission-30

---
+++ Data Structures & Algorithms/is-anagram/submission-30.ts
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        return [...s].sort().join('') === [...t].sort().join('')
+    }
+}
